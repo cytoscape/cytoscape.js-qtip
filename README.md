b581bf094a7d1dfa04a174326bc22db4a5a9dc14
Max <maxkfranz@gmail.com>
2014-05-13 17:10:25 -0400

    initial checkin of wrapper

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
 cytoscape.js-qtip
 =================
 
-A Cytoscape.js extension that wraps the QTip jQuery library
+![Preview](TODO URL)
+
+
+## Description
+
+A Cytoscape.js extension that wraps the [qTip jQuery library](http://qtip2.com)
+
+
+
+## Dependencies
+
+ * jQuery >= 1.10.0, as qTip requires it
+ * qTip >= 2.2.0
+ * Cytoscape.js >=2.2.0
+
+
+## API
+
+This plugin wraps the qTip API so it can be used on Cytoscape.js graph elements instead of HTML DOM elements.
+
+You can call qTip on graph elements:
+```js
+eles.qtip({ /* options ... */ });
+```
+
+You can call qTip on the core:
+```js
+cy.qtip({ /* options ... */ });
+```
+
+See the [qTip API for details on configuration](http://qtip2.com/options).
+
+
+## API additions
+
+These are additional options you can use for convenience.  Remember: This extension is just a wrapper to qTip.  If you require more complex behaviour, you should manually use the [qTip API](http://qtip2.com).
+
+ * `options.position.adjust.cyViewport` : When `true`, updates element qTip position on zoom and pan.  Note you'll need your own mechanism to hide out-of-bounds qTips, such as customising the parent container.
+
+  * `options.show.cyBgOnly` : When `true`, shows core qTips only for events originated on the core (not bubbled).
---
+++ jquery.cytoscape.js-qtip.js
@@ -0,0 +1,160 @@
+;(function( $, $$ ){
+  
+  function generateOpts( qtip, passedOpts ){
+    var opts = $.extend( {}, passedOpts );
+
+    if( !opts.id ){
+      opts.id = 'cy-qtip-target-' + ( +new Date() );
+    }
+
+    if( !qtip.$domEle ){
+      qtip.$domEle = $('<div id="' + opts.id + '"></div>');
+
+      var parent = passedOpts && passedOpts.position && passedOpts.position.container ? passedOpts.position.container : document.body;
+
+      $(parent).append( qtip.$domEle );
+    }
+
+    // qtip should be positioned relative to cy dom container
+    opts.position = opts.position || {};
+
+    opts.position.adjust = opts.position.adjust || {};
+
+    if( opts.position.adjust.mouse === undefined ){
+      opts.position.adjust.mouse = false;
+    }
+
+    // default show event
+    opts.show = opts.show || {};
+
+    if( !opts.show.event ){
+      opts.show.event = 'tap';
+    }
+
+    // default hide event
+    opts.hide = opts.hide || {};
+
+    if( !opts.hide.event ){
+      opts.hide.event = 'unfocus';
+    }
+
+    return opts;
+  }
+
+  function tryQtipApi( ele, passedOpts, args ){
+    // call qtip api function
+    if( $$.is.string( passedOpts ) ){
+      var ele = eles[0];
+      var qtip = ele.scratch().qtip;
+
+      if( ele && qtip ){
+        return qtip.$domEle.qtip.apply( qtip.$domEle, arguments );
+      }
+    }
+
+    throw 'Could not run qtip api';
+  }
+
+  $$('collection', 'qtip', function( passedOpts ){
+    var args = arguments;
+    var eles = this;
+    var cy = this.cy();
+    var container = cy.container();
+
+    try {
+     return tryQtipApi( ele, passedOpts, args );
+    } catch( err ){
+      // just continue
+    }
+
+    eles.each(function(i, ele){
+      var scratch = ele.scratch();
+      var qtip = scratch.qtip = scratch.qtip || {};
+      var opts = generateOpts( qtip, passedOpts );
+
+
+      // call qtip on dummy dom ele      
+      qtip.$domEle.qtip( opts );
+      var qtipApi = qtip.$domEle.qtip('api');
+
+      var updatePosition = function(e){
+        var pos = ele.renderedPosition() || e.cyRenderedPosition;
+        var cOff = container.getBoundingClientRect();
+        var w = ele.isNode() ? ele.renderedWidth() : 0;
+        var h = ele.isNode() ? ele.renderedHeight() : 0;
+
+        qtip.$domEle.css({
+          position: 'absolute',
+          zIndex: '-1',
+          width:  w  + 'px',
+          height: h + 'px',
+          left: cOff.left + pos.x + window.scrollX - w/2,
+          top: cOff.top + pos.y + window.scrollY - h/2
+        });
+      };
+
+      ele.on( opts.show.event, function(e){
+        updatePosition(e);
+
+        qtipApi.show();
+      } );
+
+      if( opts.position.adjust.cyViewport ){
+        cy.on('pan zoom', function(e){
+          updatePosition(e);
+
+          qtipApi.reposition();
+        });
+      }
+
+    });
+
+    return this; // chainability
+    
+  });
+
+  $$('core', 'qtip', function( passedOpts ){
+    var args = arguments;
+    var cy = this;
+    var container = cy.container();
+
+    try {
+     return tryQtipApi( cy, passedOpts, args );
+    } catch( err ){
+      // just continue
+    }
+
+    var scratch = cy.scratch();
+    var qtip = scratch.qtip = scratch.qtip || {};
+    var opts = generateOpts( qtip, passedOpts );
+
+
+    // call qtip on dummy dom ele      
+    qtip.$domEle.qtip( opts );
+    var qtipApi = qtip.$domEle.qtip('api');
+
+    var updatePosition = function(e){
+      var pos = e.cyRenderedPosition;
+      var cOff = container.getBoundingClientRect();
+
+      qtip.$domEle.css({
+        position: 'absolute',
+        zIndex: '-1',
+        left: cOff.left + pos.x + window.scrollX,
+        top: cOff.top + pos.y + window.scrollY
+      });
+    };
+
+    cy.on( opts.show.event, function(e){
+      if( !opts.show.cyBgOnly || (opts.show.cyBgOnly && e.cyTarget === cy) ){
+        updatePosition(e);
+
+        qtipApi.show();
+      }
+    } );
+
+    return this; // chainability
+    
+  });
+  
+})( jQuery, cytoscape );
