57fef132d78ce18a3da5d1c5fe4f74d1646893ec
Max Franz <maxkfranz@gmail.com>
2015-10-13 14:36:42 -0400

    remove meteor package; use npm

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('default', ['publish'], function( next ){
 });
 
 gulp.task('publish', [], function( next ){
-  runSequence('confver', /*'lint',*/ 'pkgver', 'push', 'tag', 'npm', 'spm', 'meteor', next);
+  runSequence('confver', /*'lint',*/ 'pkgver', 'push', 'tag', 'npm', 'spm', next);
 });
 
 gulp.task('confver', ['version'], function(){
@@ -44,7 +44,7 @@ gulp.task('version', function( next ){
     console.log('Using version number `%s` for building', version);
     next();
   }
-  
+
 });
 
 gulp.task('pkgver', ['version'], function(){
@@ -71,10 +71,6 @@ gulp.task('tag', shell.task([
 
 gulp.task('npm', shell.task([
   'npm publish .'
-]));
-
-gulp.task('meteor', shell.task([
-  'meteor publish'
 ]));
 
 gulp.task('spm', shell.task([
