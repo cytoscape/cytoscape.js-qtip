a2e768629a9667ea2357a85b93f56902fecb1f4e
Manfred Cheung <mj3cheun@uwaterloo.ca>
2017-04-04 17:50:06 -0400

    Removed spm

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('default', ['publish'], function( next ){
 });
 
 gulp.task('publish', [], function( next ){
-  runSequence('confver', /*'lint',*/ 'pkgver', 'push', 'tag', 'npm', 'spm', next);
+  runSequence('confver', /*'lint',*/ 'pkgver', 'push', 'tag', 'npm', next);
 });
 
 gulp.task('confver', ['version'], function(){
@@ -71,10 +71,6 @@ gulp.task('tag', shell.task([
 
 gulp.task('npm', shell.task([
   'npm publish .'
-]));
-
-gulp.task('spm', shell.task([
-  'spm publish'
 ]));
 
 // http://www.jshint.com/docs/options/
